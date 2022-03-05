497086dd77d098c0e3091d7b9a1041a8e814e6a3
doomerplus <42709945+doomerplus@users.noreply.github.com>
2022-03-05 23:28:02 +0800

    Update macOS-Karabiner.sh

--- macOS-Karabiner.sh
+++ macOS-Karabiner.sh
@@ -6,14 +6,13 @@ echo '{' >> rule.json
 echo '  "title": "Rules",' >> rule.json
 echo '  "rules": [' >> rule.json
 
-
 echo '    {' >> rule.json
 echo '      "description": "emacs-like",' >> rule.json
 echo '      "manipulators": [' >> rule.json
 
-from_keys=("open_bracket" "h" "p" "n" "b" "f" "a" "e")
-to_keys=("escape" "delete_or_backspace" "up_arrow" "down_arrow" "left_arrow" "right_arrow" "home" "end")
-for (( i=0; i<8; ++i)); do
+from_keys=("open_bracket" "h" "p" "n" "b" "f")
+to_keys=("escape" "delete_or_backspace" "up_arrow" "down_arrow" "left_arrow" "right_arrow")
+for (( i=0; i<6; ++i)); do
 echo '        {' >> rule.json
 echo '          "type": "basic",' >> rule.json
 echo '          "from": {' >> rule.json
@@ -41,6 +40,36 @@ if [[ "${from_keys[$i]}" =~ ^(p|n|b|f|a|e)$ ]]; then
 else
 	echo '          ]' >> rule.json
 fi
+echo '        },' >> rule.json
+done
+
+from_keys=("a" "e")
+to_keys=("left_arrow" "right_arrow")
+for (( i=0; i<2; ++i)); do
+echo '        {' >> rule.json
+echo '          "type": "basic",' >> rule.json
+echo '          "from": {' >> rule.json
+echo '            "key_code": "'${from_keys[$i]}'",' >> rule.json
+echo '            "modifiers": {' >> rule.json
+echo '              "mandatory": [' >> rule.json
+echo '                "left_command"' >> rule.json
+echo '              ]' >> rule.json
+echo '            }' >> rule.json
+echo '          },' >> rule.json
+echo '          "to": [' >> rule.json
+echo '            {' >> rule.json
+echo '              "key_code": "'${to_keys[$i]}'",' >> rule.json
+echo '              "modifiers": ["left_command"]' >> rule.json
+echo '            }' >> rule.json
+echo '          ],' >> rule.json
+echo '          "conditions": [' >> rule.json
+echo '            {' >> rule.json
+echo '              "type": "frontmost_application_unless",' >> rule.json
+echo '              "bundle_identifiers": [' >> rule.json
+echo '                "^com\\.apple\\.Terminal", "^com\\.microsoft\\.VSCode"' >> rule.json
+echo '              ]' >> rule.json
+echo '            }' >> rule.json
+echo '          ]' >> rule.json
 echo '        },' >> rule.json
 done
 
@@ -148,7 +177,7 @@ echo '          },' >> rule.json
 echo '          "to": [' >> rule.json
 echo '            {' >> rule.json
 echo '              "key_code": "delete_or_backspace",' >> rule.json
-echo '              "modifiers": ["fn", "left_control"]' >> rule.json
+echo '              "modifiers": ["fn", "left_option"]' >> rule.json
 echo '            }' >> rule.json
 echo '          ],' >> rule.json
 echo '          "conditions": [' >> rule.json
@@ -175,7 +204,7 @@ echo '          },' >> rule.json
 echo '          "to": [' >> rule.json
 echo '            {' >> rule.json
 echo '              "key_code": "delete_or_backspace",' >> rule.json
-echo '              "modifiers": ["left_control"]' >> rule.json
+echo '              "modifiers": ["left_option"]' >> rule.json
 echo '            }' >> rule.json
 echo '          ],' >> rule.json
 echo '          "conditions": [' >> rule.json
@@ -232,14 +261,13 @@ echo '            }' >> rule.json
 echo '          ]' >> rule.json
 echo '        }' >> rule.json
 
-
 echo '			]  ' >> rule.json
 echo '		},   ' >> rule.json
 echo '    {' >> rule.json
 echo '      "description": "command -> meta (terminal)",' >> rule.json
 echo '      "manipulators": [' >> rule.json
 for key in {a..z}; do
-	if [[ "$key" =~ ^(c|q)$ ]]; then
+	if [[ "$key" =~ ^(c|q|w)$ ]]; then
 		continue
 	fi
 	echo '        {' >> rule.json
@@ -269,7 +297,7 @@ for key in {a..z}; do
 	echo '            {' >> rule.json
 	echo '              "type": "frontmost_application_if",' >> rule.json
 	echo '              "bundle_identifiers": [' >> rule.json
-	echo '                "^com\\.apple\\.Terminal", "^com\\.microsoft\\.VSCode"' >> rule.json
+	echo '                "^com\\.apple\\.Terminal"' >> rule.json
 	echo '              ]' >> rule.json
 	echo '            }' >> rule.json
 	echo '          ]' >> rule.json
@@ -279,7 +307,6 @@ for key in {a..z}; do
 		echo '        },' >> rule.json
 	fi
 done
-
 
 echo '      ]' >> rule.json
 echo '    }' >> rule.json
